5390a39a3d2ba530d823629ee5665ff351db97a1
iamlen99 <allblue2903@gmail.com>
2025-06-11 20:50:26 +0700

    Upload bài tập session 03 - 3

---
+++ EX3.sql
@@ -0,0 +1,18 @@
+USE `session03_exercise`;
+
+CREATE TABLE `students` (
+	`student_id` INT NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
+	`age` INT NOT NULL,
+	`major` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`student_id`)
+);
+
+INSERT INTO `students`
+VALUES 
+(1,  'Alice', 20, 'Computer Science'),
+(2,  'Bob', 22, 'Mathematics'),
+(3,  'Charlie', 21, 'Physics');
+
+DELETE FROM `students` 
+WHERE (`student_id` = 1);
