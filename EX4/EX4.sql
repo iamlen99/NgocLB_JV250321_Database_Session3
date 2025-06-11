abba22f922f3ce42fdec723b74ee8effec1e9dda
iamlen99 <allblue2903@gmail.com>
2025-06-11 20:59:12 +0700

    Upload bài tập session 03 - 4

---
+++ EX4/EX4.sql
@@ -0,0 +1,16 @@
+USE `session03_exercise`;
+
+CREATE TABLE `students` (
+	`student_id` INT NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
+	`age` INT NOT NULL,
+	`major` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`student_id`)
+);
+
+INSERT INTO `students`
+VALUES 
+(1,  'Alice', 20, 'Computer Science'),
+(2,  'Bob', 22, 'Mathematics'),
+(3,  'Charlie', 21, 'Physics');
+
